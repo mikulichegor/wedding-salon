7378748aeed8912a294e22c99727fcfeecd20a11
Egor Mikulich <66427846+mikulichegor@users.noreply.github.com>
2022-05-11 22:07:30 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,23 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> mikulichegor@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> Minsk, Belarus - 220013 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> created by Mikulich Egor | all rights reserved </p>
+
+</section>
